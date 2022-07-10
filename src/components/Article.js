ebc7ba318d52b94caa45f609bb56394b43466686
DandysByd <bydzak@gmail.com>
2022-07-10 17:12:40 +0200

    added link to ebay

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Article = (props) => {
     return (
-        <a className={'article ' + 'article__' + props.articleName} href={props.url}>
+        <a target='_blank' className={'article ' + 'article__' + props.articleName} href={props.url}>
             <section className='article__text-holder'>
                 <h2 className='article__header'>{props.header}</h2>
             </section>
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -13,7 +13,7 @@ const Articles = () => {
             </div>
             <div className='articles__second'>
                 <Article articleName='guides' url='#' header='How to create bonsai' />
-                <Article articleName='tools' url='#' header='Tools' />
+                <Article articleName='tools' url='https://www.ebay.com/b/Bonsai-Scissor-Bonsai-Tools/75664/bn_7115963491?mkevt=1&siteid=1&mkcid=2&mkrid=711-153320-877673-6&source_name=google&mktype=dsa&campaignid=16573845087&groupid=139820097252&crlp=587861631591&keyword=&targeted=dsa-19959388920&MT_ID=&adpos=&device=c&googleloc=9062774&geo_id=56&gclid=Cj0KCQjw8amWBhCYARIsADqZJoWdwIySyTwu7i5mZ3X8Z-dIUGtPrtXHoWXguffAN4iZnVoF7YRrZH8aAvMIEALw_wcB' header='Tools' />
             </div>
         </div>
     )
